11a55ae572d5ef49577fc4d9ea9b69fe3984dd6f
Ana Carolina <aanaakaarolina@gmail.com>
2025-12-03 15:42:50 -0300

    removendo subtree lista-04 com squash

--- psc-lista-04-2025/NumerosImpares.java
+++
@@ -1,9 +0,0 @@
-public class NumerosImpares {
-    public static void main(String[] args) {
-        for(int num = 1; num <= 50; num++){
-            if(num % 2 != 0){
-                System.out.println(num);
-            }
-        }
-    }
-}
